a2de8391d291668f27263f4e3d0622e1bfdbe017
Paul Farnell <48354391+dotgrid@users.noreply.github.com>
2023-06-16 10:26:18 +0100

    Adds OS X code signing

--- forge.config.js
+++ forge.config.js
@@ -1,5 +1,13 @@
 module.exports = {
-  packagerConfig: {},
+  packagerConfig: {
+    osxSign: {}, // object must exist even if empty
+    osxNotarize: {
+      tool: 'notarytool',
+      appleId: process.env.APPLE_ID,
+      appleIdPassword: process.env.APPLE_PASSWORD,
+      teamId: process.env.APPLE_TEAM_ID,
+    },
+  },
   rebuildConfig: {},
   makers: [
     {
